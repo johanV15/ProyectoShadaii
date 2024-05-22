e2ca96171190d2bbe26771b920bd5350b03cc4fe
johanV15 <johanventura15@gmail.com>
2024-05-21 23:33:42 -0500

    Primer avance plantillas

--- app-shadaii/src/app.module.ts
+++ app-shadaii/src/app.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [],
+  controllers: [AppController],
 })
 export class AppModule {}
